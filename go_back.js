0c1f1e3e37bf4393dd0dd55485e5d3a43c2c88ad
Cese424 <alexandresemiatzh@gmail.com>
2022-04-12 17:55:15 -0300

    go back?

---
+++ go_back.js
@@ -0,0 +1,30 @@
+function App() {
+
+	var theButton;
+	var spans;
+	var currentSpan = 0;
+
+	this.init = function () {
+
+		theButton = document.querySelector('.the-button');
+		spans = document.querySelectorAll('.background span');
+
+		bindElements();
+	}
+
+	var bindElements = function () {
+		setInterval(animateSubliminalSpans, 1000);
+	}
+
+	var animateSubliminalSpans = function () {
+
+		spans[currentSpan].className = "";
+		currentSpan++;
+		currentSpan %= spans.length;
+		spans[currentSpan].className = "fade";
+	}
+
+}
+
+var app = new App();
+app.init();
